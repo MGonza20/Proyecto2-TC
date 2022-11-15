61dc2bb5a544c5b8018e3c808fd909f8de064224
MGonza20 <sara.paguaga@gmail.com>
2022-11-14 21:13:09 -0600

    Definiendo numerales de church

---
+++ inciso2.py
@@ -0,0 +1,15 @@
+
+cero = lambda f: lambda x: x
+uno = lambda f: lambda x: f(x)
+dos = lambda f: lambda x: f(f(x))
+tres = lambda f: lambda x: f(f(f(x)))
+cuatro = lambda f: lambda x: f(f(f(f(x))))
+cinco = lambda f: lambda x: f(f(f(f(f(x)))))
+seis = lambda f: lambda x: f(f(f(f(f(f(x))))))
+siete = lambda f: lambda x: f(f(f(f(f(f(f(x)))))))
+ocho = lambda f: lambda x: f(f(f(f(f(f(f(f(x))))))))
+nueve = lambda f: lambda x: f(f(f(f(f(f(f(f(f(x)))))))))
+diez = lambda f: lambda x: f(f(f(f(f(f(f(f(f(f(x))))))))))
+
+
+
