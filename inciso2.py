b1ad0e555d098453efcb251059f82882b5a81268
MGonza20 <sara.paguaga@gmail.com>
2022-11-22 16:38:08 -0600

    Implementing more examples

--- inciso2.py
+++ inciso2.py
@@ -1,6 +1,4 @@
 
-
-
 cero = lambda f: lambda x: x
 uno = lambda f: lambda x: f(x)
 dos = lambda f: lambda x: f(f(x))
@@ -14,15 +12,40 @@ nueve = lambda f: lambda x: f(f(f(f(f(f(f(f(f(x)))))))))
 diez = lambda f: lambda x: f(f(f(f(f(f(f(f(f(f(x))))))))))
 
 sucesor = lambda n: lambda f: lambda x: f(n(f)(x))
-
 suma = lambda a: lambda b: lambda f: lambda x: a(f)(b(f)(x))
 multiplicacion = lambda a: lambda b: lambda f: lambda x: a(b(f))(x)
+potencia = lambda a: lambda b: lambda f: lambda x: b(a)(f)(x)
+
+# Deben ser iguales
+print(sucesor(uno)(lambda x: x + 1)(1))
+print(dos(lambda x: x + 1)(1))
+
+# Deben ser iguales
+print(sucesor(cuatro)(lambda x: x + 1)(1))
+print(cinco(lambda x: x + 1)(1))
+
+# Deben de ser iguales
+print(suma(uno)(tres)(lambda x: x + 1)(1))
+print(cuatro(lambda x: x + 1)(1))
+
+# Deben de ser iguales
+print(suma(cinco)(cuatro)(lambda x: x + 1)(1))
+print(nueve(lambda x: x + 1)(1))
 
 # Deben ser iguales
 print(multiplicacion(dos)(uno)(lambda x: x + 1)(1))
 print(dos(lambda x: x + 1)(1))
 
+# Deben ser iguales
+print(multiplicacion(tres)(tres)(lambda x: x + 1)(1))
+print(nueve(lambda x: x + 1)(1))
+
 # Deben de ser iguales
-print(suma(uno)(tres)(lambda x: x + 1)(1))
-print(cuatro(lambda x: x + 1)(1))
+print(potencia(uno)(cero)(lambda x: x + 1)(1))
+print(uno(lambda x: x + 1)(1))
+
+# Deben de ser iguales
+print(potencia(dos)(tres)(lambda x: x + 1)(1))
+print(ocho(lambda x: x + 1)(1))
+
 
