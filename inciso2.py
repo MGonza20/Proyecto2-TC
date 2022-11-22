4fa9e308fff01427e0dfaa618b45dc035232d42d
Guillermo Santos <guillermo.santosba@gmail.com>
2022-11-22 15:55:21 -0600

    Finalizando ejercicio con ejemplos

--- inciso2.py
+++ inciso2.py
@@ -13,26 +13,16 @@ ocho = lambda f: lambda x: f(f(f(f(f(f(f(f(x))))))))
 nueve = lambda f: lambda x: f(f(f(f(f(f(f(f(f(x)))))))))
 diez = lambda f: lambda x: f(f(f(f(f(f(f(f(f(f(x))))))))))
 
-# numbers = {
-# 	0: cero,
-# 	1: uno,
-# 	2: dos,
-# 	3: tres,
-# 	4: cuatro,
-# 	5: cinco,
-# 	6: seis,
-# 	7: siete,
-# 	8: ocho,
-# 	9: nueve,
-# 	10: diez
-# }
-
-
 sucesor = lambda n: lambda f: lambda x: f(n(f)(x))
-potencia = lambda a: lambda b: lambda f: lambda x: b(a)(f)(x)
-# suma = lambda a, b: numbers[a+b]
-# multiplicacion = lambda a,b: numbers[a*b]
+
 suma = lambda a: lambda b: lambda f: lambda x: a(f)(b(f)(x))
 multiplicacion = lambda a: lambda b: lambda f: lambda x: a(b(f))(x)
 
+# Deben ser iguales
 print(multiplicacion(dos)(uno)(lambda x: x + 1)(1))
+print(dos(lambda x: x + 1)(1))
+
+# Deben de ser iguales
+print(suma(uno)(tres)(lambda x: x + 1)(1))
+print(cuatro(lambda x: x + 1)(1))
+
