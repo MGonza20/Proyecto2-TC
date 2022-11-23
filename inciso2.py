09ee463e4a8d3b89ab20d932e8b6c1677d54d00a
Guillermo Santos <guillermo.santosba@gmail.com>
2022-11-23 08:04:20 -0600

    Added examples

--- inciso2.py
+++ inciso2.py
@@ -16,7 +16,9 @@ suma = lambda a: lambda b: lambda f: lambda x: a(f)(b(f)(x))
 multiplicacion = lambda a: lambda b: lambda f: lambda x: a(b(f))(x)
 potencia = lambda a: lambda b: lambda f: lambda x: b(a)(f)(x)
 
+print("Alpha")
 # Deben ser iguales
+print("Sucesor")
 print(sucesor(uno)(lambda x: x + 1)(1))
 print(dos(lambda x: x + 1)(1))
 
@@ -24,6 +26,8 @@ print(dos(lambda x: x + 1)(1))
 print(sucesor(cuatro)(lambda x: x + 1)(1))
 print(cinco(lambda x: x + 1)(1))
 
+
+print("Suma")
 # Deben de ser iguales
 print(suma(uno)(tres)(lambda x: x + 1)(1))
 print(cuatro(lambda x: x + 1)(1))
@@ -31,6 +35,8 @@ print(cuatro(lambda x: x + 1)(1))
 # Deben de ser iguales
 print(suma(cinco)(cuatro)(lambda x: x + 1)(1))
 print(nueve(lambda x: x + 1)(1))
+
+print("Multiplicacion")
 
 # Deben ser iguales
 print(multiplicacion(dos)(uno)(lambda x: x + 1)(1))
@@ -40,6 +46,8 @@ print(dos(lambda x: x + 1)(1))
 print(multiplicacion(tres)(tres)(lambda x: x + 1)(1))
 print(nueve(lambda x: x + 1)(1))
 
+print("Potencia")
+
 # Deben de ser iguales
 print(potencia(uno)(cero)(lambda x: x + 1)(1))
 print(uno(lambda x: x + 1)(1))
@@ -48,4 +56,45 @@ print(uno(lambda x: x + 1)(1))
 print(potencia(dos)(tres)(lambda x: x + 1)(1))
 print(ocho(lambda x: x + 1)(1))
 
+
+# Beta
+print("Beta")
+# Deben ser iguales
+print("Sucesor")
+print(sucesor(uno)(lambda x: x * 2)(1))
+print(dos(lambda x: x * 2)(1))
+
+# Deben ser iguales
+print(sucesor(cuatro)(lambda x: x * 2)(1))
+print(cinco(lambda x: x * 2)(1))
+
+print("Suma")
+
+# Deben de ser iguales
+print(suma(uno)(tres)(lambda x: x * 2)(1))
+print(cuatro(lambda x: x * 2)(1))
+
+# Deben de ser iguales
+print(suma(cinco)(cuatro)(lambda x: x * 2)(1))
+print(nueve(lambda x: x * 2)(1))
+
+# Deben ser iguales
+print(multiplicacion(dos)(uno)(lambda x: x * 2)(1))
+print(dos(lambda x: x * 2)(1))
+p
+rint("Multiplicacion")
+
+# Deben ser iguales
+print(multiplicacion(tres)(tres)(lambda x: x * 2)(1))
+print(nueve(lambda x: x * 2)(1))
+
+
+print("Potencia")
+# Deben de ser iguales
+print(potencia(uno)(cero)(lambda x: x * 2)(1))
+print(uno(lambda x: x * 2)(1))
+
+# Deben de ser iguales
+print(potencia(dos)(tres)(lambda x: x * 2)(1))
+print(ocho(lambda x: x * 2)(1))
 
